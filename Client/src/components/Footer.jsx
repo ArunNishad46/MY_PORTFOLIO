e6ed6fb4ebb8ac96ea19ffa8e82c6fa0deb312f1
Arun Nishad <157214616+ArunNishad46@users.noreply.github.com>
2025-12-08 17:56:43 +0530

    Add heart icon to footer copyright message

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { MdOutlineMail } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="py-4 border-t border-slate-700 mt-4">
       <div className="flex flex-col md:flex-row items-center justify-center md:gap-6 gap-2 px-2">
         <p className="text-sm text-slate-400">
-          © {new Date().getFullYear()} Arun. All rights reserved.
+          <FaHeart className="text-md text-red-600" /> made by Arun. Copyright © {new Date().getFullYear()}
         </p>
       </div>
     </footer>
