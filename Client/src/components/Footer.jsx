d00ca103db34f48476c538260bf905817f51e18b
Arun Nishad <157214616+ArunNishad46@users.noreply.github.com>
2025-12-08 17:58:12 +0530

    Change paragraph to flex layout in Footer component

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="py-4 border-t border-slate-700 mt-4">
       <div className="flex flex-col md:flex-row items-center justify-center md:gap-6 gap-2 px-2">
-        <p className="text-sm text-slate-400">
+        <p className="flex text-sm text-slate-400">
           <FaHeart className="text-md text-red-600" /> made by Arun. Copyright © {new Date().getFullYear()}
         </p>
       </div>
